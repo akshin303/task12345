3509917a67f678329a0352d39cc206484bc32335
akshin303 <119412774+akshin303@users.noreply.github.com>
2022-12-14 13:16:20 +0400

    Add files via upload

---
+++ ev tapshirigi.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //- Verilmiş yazıda rəqəm olub olmadığını qaytaran metod
+            var word = Console.ReadLine();
+          
+            Console.WriteLine(Hasworld(word));
+
+        }
+        static bool Hasworld(string world)
+        {
+            for(int i=0;i<world.Length;i++)
+            {
+                if (world[i] == '0' || world[i] == '1'|| world[i] == '2'|| world[i] == '3' || world[i] == '4' || world[i] == '5'|| world[i] == '6' || world[i] == '7' || world[i] == '8' || world[i] == '9')
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
