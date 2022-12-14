c375da17d6823271d8de971a28e7af0b57e3bd95
akshin303 <119412774+akshin303@users.noreply.github.com>
2022-12-14 13:37:36 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string[] world = new string[] { "salam", "sa2", "sa4", "sa23" } ;
+           
+            Console.WriteLine(Count(world));
+        }
+        //Verilmiş yazılar siyahısında neçə yazıda rəqəm olduğunu qaytaran metod
+        static int Count(string[]names)
+        {
+            var count = 0;
+            
+            for(int i =0;i<names.Length;i++)
+            {
+                if (names[i] == "0" || names[i] == "1" || names[i] == "2" || names[i] == "3"|| names[i] == "4"|| names[i] == "5"|| names[i] == "6" || names[i] == "7" || names[i] == "8" || names[i] == "9")
+                {
+                    break;
+                    
+                }
+                count++;
+                
+            }
+            return count;
+        }
+    }
+}
